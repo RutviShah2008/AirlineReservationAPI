aff6bb9c8bcd7cce989de73f31d207744fda0ede
RutviShah2008 <200390373@student.georgianc.on.ca>
2019-04-08 03:25:00 -0400

    Added Get,Post,Put and Delete method

--- AirlineReservationAPI/Controllers/FlightController.cs
+++ AirlineReservationAPI/Controllers/FlightController.cs
@@ -1,9 +1,9 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using AirlineReservationAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -28,28 +28,66 @@ namespace AirlineReservationAPI.Controllers
 
         // GET api/<controller>/5
         [HttpGet("{id}")]
-        public Flight Get(int id)
+        public ActionResult Get(int id)
         {
             var flight = db.Flights.SingleOrDefault(f => f.FlightID == id);
-            return flight;
+            if(flight == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(flight);
+            }            
         }
 
         // POST api/<controller>
         [HttpPost]
-        public void Post([FromBody]string value)
+        public ActionResult Post([FromBody]Flight flight)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                db.Flights.Add(flight);
+                db.SaveChanges();
+                return CreatedAtAction("Post", new { id = flight.FlightID }, flight);
+            }
         }
 
         // PUT api/<controller>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public ActionResult Put(int id, [FromBody]Flight flight)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                db.Entry(flight).State = EntityState.Modified;
+                db.SaveChanges();
+                return AcceptedAtAction("Put", new { id = flight.FlightID }, flight);
+            }
         }
 
         // DELETE api/<controller>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult Delete(int id)
         {
+            var flight = db.Flights.SingleOrDefault(f => f.FlightID == id);
+            if (flight == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                db.Flights.Remove(flight);
+                db.SaveChanges();
+                return Ok();
+            }
         }
     }
 }
