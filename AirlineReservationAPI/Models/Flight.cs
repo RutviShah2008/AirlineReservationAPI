c90c9a3bd710344f7d944bbe63989acef98022bf
RutviShah2008 <200390373@student.georgianc.on.ca>
2019-04-09 01:42:54 -0400

    Removed Required notation

--- AirlineReservationAPI/Models/Flight.cs
+++ AirlineReservationAPI/Models/Flight.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,8 +12,7 @@ namespace AirlineReservationAPI.Models
 
         
         public DateTime? FlightDate { get; set; }
-
-
+        
         public int? FlightJetID { get; set; }
 
             
